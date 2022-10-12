f6e82ff3c90e85d473c0ca0bcdf4d51fc23a4e52
Zoni Nation <zonination@gmail.com>
2022-10-12 13:50:09 -0400

    Create rlogin.py

---
+++ rlogin.py
@@ -0,0 +1,8 @@
+def ad():
+    import praw
+    r = praw.Reddit(username='',
+                    password='',
+                    client_id='',
+                    client_secret='',
+                    user_agent='')
+    return r
